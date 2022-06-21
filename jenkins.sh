b6c26d6d9e465129566677c39d2e2f830c0c4071
Dan Geng <87605453+MilaDan@users.noreply.github.com>
2022-06-21 03:50:54 -0700

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VAGRANT_HOST_DIR=/mnt/host_machine
+
+########################
+# Jenkins & Java
+########################
+echo "Installing Jenkins and Java"
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCEF32E745F2C3D5 > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install default-jdk jenkins > /dev/null 2>&1
+echo "Installing Jenkins default user and config"
+sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/config.xml /var/lib/jenkins/
+sudo mkdir -p /var/lib/jenkins/users/admin
+sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/users/admin/config.xml /var/lib/jenkins/users/admin/
+sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
